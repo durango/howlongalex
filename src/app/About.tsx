5c856b431cd4c7002cd7244546259891c79a6364
Jonas Frost <j@duplo.org>
2020-05-15 10:30:33 -0700

    added about page

---
+++ src/app/About.tsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export class About extends Component {
+	render() {
+		return (
+			<article className="about">
+				<h3>So who is Alex? </h3>
+				<p>Alex is our good buddy, probably the smartest guy in the room, and nicest person you've ever met.</p>
+			</article>
+		)
+	}
+}
